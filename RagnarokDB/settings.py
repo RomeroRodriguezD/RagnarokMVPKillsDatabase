51e0ce2fde3a4aa62a744f267742eb27030afe47
David <davidromerorodriguez21@gmail.com>
2022-07-12 18:21:14 +0200

    allowed host

--- RagnarokDB/settings.py
+++ RagnarokDB/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-r4u5^**5(s8r0iitm1yb#rs)9u#@9h)sd4z5!e=!ed3h^jv=i#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ragnarokmvpkillsdatabase.herokuapp.com']
 
 
 # Application definition
