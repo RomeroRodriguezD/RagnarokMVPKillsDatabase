90c167e8b9bb5f31469259d9384ee952d36b6fa9
David <105886661+RomeroRodriguezD@users.noreply.github.com>
2022-07-12 19:41:19 +0200

    Update settings.py

--- RagnarokDB/settings.py
+++ RagnarokDB/settings.py
@@ -86,12 +86,8 @@ MESSAGE_TAGS = {
 
 DATABASES = {
     'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'devdi5dii9ihal',
-       'USER': 'bedujzhleveibc',
-       'PASSWORD': '2caaa71b5ac4f51e60531aab4ac217cc6b12416525c9651fba9c1d196a02418a',
-       'HOST': 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com',
-       'PORT': '5432',
+       'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
