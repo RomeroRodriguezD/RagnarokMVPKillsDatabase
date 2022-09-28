f6cce6ef2b466254b52fcb4662c97032d881a654
David <105886661+RomeroRodriguezD@users.noreply.github.com>
2022-09-28 18:41:51 +0200

    Delete djangotutorial directory

--- djangotutorial/Session.py
+++
@@ -1,5 +0,0 @@
-def sessfun(request):
-    num_visits = request.session.get('num_visits', 0) +1
-    request.session['num_visits'] = num_visits
-    if num_visits > 4 : del(request.session['num_visits'])
-    return HttpResponse('view count = ' + str(num_visits))
