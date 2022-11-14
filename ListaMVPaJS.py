403279c3b5e907135d318d37715911ac3ff6bc36
David <105886661+RomeroRodriguezD@users.noreply.github.com>
2022-11-14 21:19:58 +0100

    Delete ListaMVPaJS.py

--- ListaMVPaJS.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-csvmvps = pd.read_csv('Lista MVP.csv')
-lista_mvps = []
-
-for i in csvmvps.Amdarais:  #Creamos la lista utilizando la columna "Amdarais" del CSV.
-    lista_mvps.append(i)
-
-print(lista_mvps)
