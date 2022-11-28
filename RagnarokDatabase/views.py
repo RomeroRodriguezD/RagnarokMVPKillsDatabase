a2408bfbe96175209eb735b277387d75deaafa4a
David <105886661+RomeroRodriguezD@users.noreply.github.com>
2022-11-28 17:58:27 +0100

    Update views.py

--- RagnarokDatabase/views.py
+++ RagnarokDatabase/views.py
@@ -107,8 +107,8 @@ def mvp_database(request):
                 
                 elif "backup" in request.POST:
                     
-                    list = list(Kills.objects.all().values())
-                    return JsonResponse(list, safe=False, status=status.HTTP_200_OK)
+                    list = list(Kills.objects.get(owner=owner).values())
+                    return JsonResponse(list, safe=False)
                                       
                 else:
                     mvp = int(form.cleaned_data.get('name'))
